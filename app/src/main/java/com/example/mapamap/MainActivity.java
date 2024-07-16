de5a415d6281b7126b0ee42430d4770b4d42c6d9
SMU-S4 <dchaveza@uteq.edu.ec>
2024-07-16 09:34:13 -0500

    Finalisima

--- app/src/main/java/com/example/mapamap/MainActivity.java
+++ app/src/main/java/com/example/mapamap/MainActivity.java
@@ -11,6 +11,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mapamap.WebService.Asynchtask;
+import com.example.mapamap.WebService.WebService;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,10 +21,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.slider.Slider;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, Asynchtask {
 GoogleMap mapa;
 
 Double lat , lng ;
@@ -30,6 +42,7 @@ EditText txtLat , txtLong;
 Circle circulo=null;
 Slider sliderRadio;
 float Radio ;
+List<Marker> markers = new ArrayList<Marker>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,7 +81,7 @@ float Radio ;
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
         mapa = googleMap;
-        mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mapa.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         mapa.getUiSettings().setZoomControlsEnabled(true);
         mapa.getUiSettings().setCompassEnabled(true);
 
@@ -109,6 +122,10 @@ float Radio ;
         txtLat.setText(String.format("%.4f", lat));
         txtLong.setText(String.format("%.4f", lng));
         PintarCirculo();
+
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://turismoquevedo.com/lugar_turistico/json_getlistadoMapa?lat=" + lat+ "&lng=" + lng+"&radio=" + (Radio/10.0) ,datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET");
     }
 
     private void PintarCirculo(){
@@ -126,4 +143,22 @@ float Radio ;
     }
 
 
+    @Override
+    public void processFinish(String result) throws JSONException {
+
+
+        for(Marker marker: markers) marker.remove();
+        markers.clear();
+
+        List<Marker> markers= new ArrayList<Marker>();
+        JSONObject JSONobj= new JSONObject(result);
+        JSONArray jsonLista= JSONobj.getJSONArray("data");
+        for(int i=0; i< jsonLista.length(); i++) {
+            JSONObject lugar = jsonLista.getJSONObject(i);
+            markers.add(mapa.addMarker(
+                    new MarkerOptions().position(
+                            new LatLng(lugar.getDouble("lat"), lugar.getDouble("lng"))
+                    ).title(lugar.get("nombre").toString())));
+        }
+    }
 }
